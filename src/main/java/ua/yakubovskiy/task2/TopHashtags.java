285ca4e2010b8835e19192eaf740b48872f1656e
Gamer <nelo9997@gmai.com>
2022-11-09 15:30:24 +0200

    editing tests

--- src/main/java/ua/yakubovskiy/task2/TopHashtags.java
+++ src/main/java/ua/yakubovskiy/task2/TopHashtags.java
@@ -7,18 +7,18 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class TopHashtags {
+
     public Map<String, Long> chooseTop(List<String> lines){
         if(lines == null) throw new NullPointerException("list must not be null");
         if(lines.isEmpty()) throw new IllegalArgumentException("list must not be empty");
-        Map<String, Long> result = new LinkedHashMap<>();
-        lines.stream()
+        return lines.stream()
                 .filter(s -> s.startsWith("#"))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream()
                 .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
                 .limit(5)
-                .forEach(stringLongEntry -> result.put(stringLongEntry.getKey(), stringLongEntry.getValue()));
-        return result;
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (x, y) -> y, LinkedHashMap::new));
     }
+
 }
